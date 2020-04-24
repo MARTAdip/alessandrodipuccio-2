9dd2399c0d84d49079c43e65e5e5fdc9ff442fb2
marta <martadipuccio@me.com>
2020-04-24 18:57:48 +0200

    fixed err

--- components/fullpage/Fullpage.js
+++ components/fullpage/Fullpage.js
@@ -1,16 +1,13 @@
-import React, { useRef } from "react";
-import AwesomeSlider from "react-awesome-slider";
-import "react-awesome-slider/dist/custom-animations/cube-animation.css";
-import "react-awesome-slider/dist/custom-animations/fall-animation.css";
-import "react-awesome-slider/dist/custom-animations/fold-out-animation.css";
-import "react-awesome-slider/dist/custom-animations/scale-out-animation.css";
-import "react-awesome-slider/dist/custom-animations/open-animation.css";
-import {
-  withNavigationHandlers,
-  withNavigationContext
-} from "react-awesome-slider/dist/navigation";
-import { media } from "./media";
-import Startup from "../startup/startup";
+import React, { useRef } from 'react';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import 'react-awesome-slider/dist/custom-animations/fall-animation.css';
+import 'react-awesome-slider/dist/custom-animations/fold-out-animation.css';
+import 'react-awesome-slider/dist/custom-animations/scale-out-animation.css';
+import 'react-awesome-slider/dist/custom-animations/open-animation.css';
+import { withNavigationHandlers, withNavigationContext } from 'react-awesome-slider/dist/navigation';
+import { media } from './media';
+import Startup from '../startup/Startup';
 
 const Slider = withNavigationHandlers(AwesomeSlider);
 
@@ -27,8 +24,8 @@ export default withNavigationContext(({ fullpage }) => {
       onTransitionEnd={() => {
         // HANDLE THE PAGE ELEMENTS ANIMATION ON FIRST TRANSITION END
         if (isFirstLoad.current === true) {
-          document.querySelector("body").classList.add("animated");
-          document.querySelector("body").classList.add("visible");
+          document.querySelector('body').classList.add('animated');
+          document.querySelector('body').classList.add('visible');
         }
       }}
       media={media}
